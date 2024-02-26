219ae632db0b931b4d291f046e1fc6dbe28b85c9
riccardo <283824@studenti.unimore.it>
2024-02-26 22:22:33 +0100

    commit

--- Network_Analysis/settings.py
+++ Network_Analysis/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "Network_Analysis.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Network_AnalysisDB',
+        'USER': 'root',
+        'PASSWORD': 'MySQL123',
+        'PORT': '3306',
+        'HOST': 'localhost',
     }
 }
 
