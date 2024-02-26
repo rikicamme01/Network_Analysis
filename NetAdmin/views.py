5b1e0c4421b80d86445a09f951d16c2643f0c6d8
riccardo <283824@studenti.unimore.it>
2024-02-26 20:54:49 +0100

    commit

---
+++ NetAdmin/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def netAdmin (request):
+    return render(request, "netAdmin/prova.html")
---
+++ NetUser/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NetUserConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "NetUser"
