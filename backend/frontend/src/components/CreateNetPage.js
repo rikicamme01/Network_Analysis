968b427e8162304e921f49891b5b3c4371ccfab4
riccardo <283824@studenti.unimore.it>
2024-10-30 19:21:08 +0100

    commit

--- backend/frontend/src/components/CreateNetPage.js
+++ backend/frontend/src/components/CreateNetPage.js
@@ -1,11 +1,11 @@
 import React, {Component} from "react";
 import { useState } from "react";
 import Button from "@mui/material/Button";
-import Grid from "@mui/material/Unstable_Grid2";
+import Grid from "@mui/material/Grid2";
 import Typography from "@mui/system/typography";
-import TextField from "@mui/material";
+import TextField from "@mui/material/TextField";
 import FormHelperText from "@mui/material";
-import FormControl from "@mui/material";
+import FormControl from "@mui/material/FormControl";
 import Radio from "@mui/material";
 import RadioGroup from "@mui/material";
 import FormControlLabel from "@mui/material";
@@ -19,8 +19,14 @@ export default function CreateNetPage(props){
     };
 
     return (
-        <Grid item xs={12} align="center">
-            <Grid item xs={12} align="center"/>
+        <Grid>
+            <Grid item xs={12} align="center">
+                <FormControl>
+                    <TextField required={true} type ='number' />
+                </FormControl>
+
+
+            </Grid>
         </Grid>
     );
 }
