acf34f7b3aac4e4fc81dafbe70bbbdd53817cb87
riccardo <283824@studenti.unimore.it>
2024-10-30 19:11:58 +0100

    commit

--- backend/frontend/src/components/CreateNetPage.js
+++ backend/frontend/src/components/CreateNetPage.js
@@ -1,18 +1,26 @@
 import React, {Component} from "react";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Unstable_Grid2";
+import Typography from "@mui/system/typography";
+import TextField from "@mui/material";
+import FormHelperText from "@mui/material";
+import FormControl from "@mui/material";
+import Radio from "@mui/material";
+import RadioGroup from "@mui/material";
+import FormControlLabel from "@mui/material";
 
-export default function CreateNetworkPage(props){
-    // Definisci lo stato per il contatore
-    const [count, setCount] = useState(0);
 
-    // Funzione per gestire il click del pulsante
-    const handleButtonClick = () => {
-        setCount(count + 1);  // Incrementa il contatore
+
+export default function CreateNetPage(props){
+    const defaultVotes = 3;
+    const createAssesment = () => {
+        pass
     };
 
     return (
-        <div>
-            <h1>Contatore: {count}</h1>
-            <button onClick={handleButtonClick}>Incrementa</button>
-        </div>
+        <Grid item xs={12} align="center">
+            <Grid item xs={12} align="center"/>
+        </Grid>
     );
 }
