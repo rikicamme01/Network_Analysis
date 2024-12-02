9159f590c2d2ffc95de30268686886adb19084fc
riccardo <283824@studenti.unimore.it>
2024-12-02 17:46:20 +0100

    commit AdminSurvey

--- backend/frontend/src/pages/admin/AdminSurvey.js
+++ backend/frontend/src/pages/admin/AdminSurvey.js
@@ -1,9 +1,53 @@
 import React from "react";
+import Layout from "../../components/Layout";
+import FasiAdmin from "../../components/FasiAdmin";
+import CustomAlert from "../../components/CustomAlert";
+import CustomButton from "../../components/CustomButton";
+import "../../../static/css/adminSurvey.css"
 
-export default function AdminSurvey(){
 
-    return( 
-        <h1>AdminSurvey</h1>
+export default function AdminSurvey() {
 
+    return (
+        <Layout
+            title="Nome_ass"
+            spallasx={<FasiAdmin status={[1, 0, 0]} />}
+            main={
+                <div className="div-genitore">
+                    <div className="div-testo">
+                        Gentile admin, ti invitiamo a rispondere alle seguenti domande. Le tue risposte sono necessarie alla personalizzazione del questionario da somministrare ai ruoli professionali che vuoi coinvolgere nell’indagine. Il questionario si genererà dopo che avrai salvato le tue risposte.
+                    </div>
+                    <div className="div-domande">
+                        Domande
+                    </div>
+                    <div className="div-terza-riga">
+                        <div className="div-salva">
+                            <div className="button-salva">
+                                <CustomButton
+                                    text='Salva'
+                                    width='220px'
+
+                                />
+                            </div>
+
+                            <div className="progress">
+                                Progress
+                            </div>
+                        </div>
+                        <div className="div-alert">
+                            <CustomAlert
+                                text='Survey salvata'
+
+
+
+                            />
+                        </div>
+
+                    </div>
+
+                </div>
+            }
+
+        />
     );
 }
